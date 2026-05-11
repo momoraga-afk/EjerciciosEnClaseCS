4c33231abf92c6b89c35b63237c7f251d1f3b9bf
MoisesMoraga <momoraga@uamv.edu.ni>
2026-05-11 07:52:46 -0700

    archivos restaurados

---
+++ magnitudVectores/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] V = new int[5];
+double sumaCuadrados = 0;
+
+Console.WriteLine("--- Cálculo de Magnitud de un Vector ---");
+Console.WriteLine("Ingrese los 5 componentes del vector:");
+
+for (int i = 0; i < V.Length; i++)
+{
+    try
+    {
+        Console.Write($"Componente {i + 1}: ");
+        V[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Entrada no válida, por favor, ingrese un número entero.");
+        Console.ResetColor();
+        i--; 
+    }
+}
+
+for (int i = 0; i < V.Length; i++)
+{
+    sumaCuadrados += Math.Pow(V[i], 2);
+}
+
+double magnitud = Math.Sqrt(sumaCuadrados);
+
+Console.WriteLine("\nLa magnitud total del vector es:");
+Console.WriteLine(magnitud);
---
+++ productEsca/Program.cs
@@ -0,0 +1,48 @@
+﻿int[] vectorA = new int[5];
+int[] vectorB = new int[5];
+int productoEscalar = 0;
+
+Console.WriteLine("--- Cálculo del Producto Escalar ---");
+
+Console.WriteLine("\nIngrese los elementos del Vector A:");
+for (int i = 0; i < vectorA.Length; i++)
+{
+    try
+    {
+        Console.Write($"Elemento A {i + 1}: ");
+        vectorA[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error: Ingrese un número entero válido.");
+        Console.ResetColor();
+        i--;
+    }
+}
+
+Console.WriteLine("\nIngrese los elementos del Vector B:");
+for (int i = 0; i < vectorB.Length; i++)
+{
+    try
+    {
+        Console.Write($"Elemento B {i + 1}: ");
+        vectorB[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error: Ingrese un número entero válido.");
+        Console.ResetColor();
+        i--;
+    }
+}
+for (int i = 0; i < vectorA.Length; i++)
+{
+    productoEscalar += vectorA[i] * vectorB[i];
+}
+
+// 5. Mostrar el producto escalar total
+Console.WriteLine("\n-------------------------------------------");
+Console.WriteLine($"El producto escalar total es: {productoEscalar}");
+Console.WriteLine("-------------------------------------------");
